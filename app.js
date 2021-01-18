0978728649335be92f4cd1ed214b0eb49de19329
Maneet Lodha <36129353+maneetlodha28@users.noreply.github.com>
2021-01-18 19:51:48 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+const app = express();
+const GMAIL_USER = "maneetlodha28@gmail.com";
+
+require("dotenv").config();
+const cors = require("cors");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.get("/", (req, res) => {
+  return res.json({ name: "maneet" });
+});
+app.post("/contact", (req, res) => {
+  // Instantiate the SMTP server
+  const smtpTrans = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: GMAIL_USER,
+      pass: "manujain@28",
+    },
+  });
+
+  // Specify what the email will look like
+  const mailOpts = {
+    from: "Your sender info here", // This is ignored by Gmail
+    to: GMAIL_USER,
+    subject: "New message from contact form at ",
+    text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`,
+  };
+
+  // Attempt to send the email
+  smtpTrans.sendMail(mailOpts, (error, response) => {
+    if (error) {
+      res.json(error); // Show a page indicating failure
+    } else {
+      res.redirect("https://fineshine.netlify.app/");
+    }
+  });
+});
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`server started at ${port}`));
